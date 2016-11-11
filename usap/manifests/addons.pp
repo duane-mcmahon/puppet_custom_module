e977d23372d25d2542c147454b1d69a4880b3659
Duane McMahon <s3116979@student.rmit.edu.au>
2016-11-10 23:55:12 -0500

    added httpd and apache2 services (untested)

--- usap/manifests/addons.pp
+++ usap/manifests/addons.pp
@@ -1,17 +1,48 @@
 class usap::addons inherits usap
 {
-    
+     package { 'openssh':
+            name    => 'openssh-server',
+            ensure  => installed
+        
+         }
+
      case $::osfamily {
         
         
         'debian': { package { 'c-shell(debian)':
                     name        => 'csh',
                     provider    => 'apt',
-                    ensure      => installed    } }
+                    ensure      => installed    } 
+                    
+                    package { 'openssh(debian)':
+                    name        => 'openssh-client',
+                    provider    => 'apt',
+                    ensure      => installed    }
+
+                    package { 'apache(debian)':
+                    name        => 'apache2',
+                    provider    => 'apt',
+                    ensure      => installed    }
+
+
+
+                    }
 
          default: { package { 'c-shell':
                     name        => 'tcsh',
-                    ensure  => installed    } }
+                    ensure      => installed        }
+
+                    package { 'openssh':
+                        name    => 'openssh-clients',
+                        ensure  => installed
+                        
+                        }
+                    
+                    package { 'apache':
+                        name    => 'httpd',
+                        ensure  => installed
+
+                    }
 
        }
 
--- usap/manifests/services.pp
+++ usap/manifests/services.pp
@@ -1,8 +1,21 @@
 class usap::services inherits usap {
+ 
+
+case $::osfamily {
+
+        'debian': { service { 'apache2':
+
+                    ensure  => running,
+                    enable  => true     }   
+
+        }
+        
+        default: { service { 'httpd':
+
+                    ensure  => running,
+                    enable  => true     }
     
-    
-    
-    
-    
-    
+        }
+}
+
 }
